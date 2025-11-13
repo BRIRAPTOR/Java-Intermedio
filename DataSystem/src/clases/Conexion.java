f9deaf3578b907038f05ebe915c9aba96d577947
Brian Hazel <167057611+BRIRAPTOR@users.noreply.github.com>
2025-11-13 15:44:42 -0600

    Actualizando Data System

--- DataSystem/src/clases/Conexion.java
+++ DataSystem/src/clases/Conexion.java
@@ -8,7 +8,7 @@ public class Conexion {
 //Conexión Local
     public static Connection conectar(){
         try {
-            Connection cn = DriverManager.getConnection("jdbc:mysql://srv1288.hstgr.io:3306/u266478322_bd_ds","u266478322_briraptor","kP2LCpt/h~^2");
+            Connection cn = DriverManager.getConnection("jdbc:mysql://server","user","password");
             return cn;
         } catch (SQLException e) {
             System.out.println("Error en la conección local" + e);
